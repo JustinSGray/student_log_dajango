d4486ff9b3232ffee859d00642d2a1f8163976ff
Justin Gray <Justin.S.Gray@gmail.com>
2012-11-16 15:59:59 -0500

    moving lots of stuff around

--- student_log/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
-
-from log.views import v1_api
-
-urlpatterns = patterns('',
-    url(r'^api/',include(v1_api.urls)),
-)
