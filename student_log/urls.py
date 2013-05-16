565102c50479a392a9bfb2f3be7d9ff94175b8cd
Justin Gray <justin.s.gray@gmail.com>
2013-05-16 16:29:40 -0400

    turning of profiler

--- student_log/urls.py
+++ student_log/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls import patterns, include, url
 # Uncomment the next two lines to enable the admin:
 
 urlpatterns = patterns('',
-    url(r'^profiler/', include('profiler.urls')),
+    #url(r'^profiler/', include('profiler.urls')),
     (r'^', include('log.urls')),
 )
